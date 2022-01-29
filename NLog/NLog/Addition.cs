a896066261d9c3f6b64b86181a954ea55a214474
shaik ameer basha <shaikshabana758@gmail.com>
2022-01-29 13:15:18 +0530

    [shabana] adding NLog

---
+++ NLog/NLog/Addition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLog
+{
+    internal class Addition
+    {
+        public static Logger logger = LogManager.GetCurrentClassLogger();
+        public static void Sum(int first, int second)
+        {
+            if (first <= 0 || second <= 0)
+            {
+                //NLog.DebugInfo("Debug Unsuccessful due to wrong inputs");
+                //NLog.ErrorInfo("Providing wrong inputs");
+                //NLog.WarnInfo("Inputs should not be zero values");
+                logger.Debug("Debug Unsuccessful due to wrong inputs");
+                logger.Error("Providing wrong logger.Error(Providing wrong inputs");
+            }
+            else
+            {
+                int sum = first + second;
+                Console.WriteLine("sum:" + sum);
+                logger.Debug("Debug successful");
+                logger.Info("Successfully caluculated sum:" + sum);
+                //NLog.DebugInfo("Debug successful");
+                //NLog.InfoLevel("Successfully caluculated sum:"+sum);
+            }
+        }
+    }
+}
---
+++ NLog/NLog/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Addition.Sum(60,60);
+            Console.ReadLine();
+
+        }
+    }
+}
